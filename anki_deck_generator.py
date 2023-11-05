dca9719c303d0553324dd2b31170018ee266385f
kota113 <iwa124816+keio@gmail.com>
2023-11-06 03:26:44 +0900

    image not included still

--- anki_deck_generator.py
+++ anki_deck_generator.py
@@ -18,7 +18,7 @@ card_model = genanki.Model(
                 'name': 'Card 1',
                 'qfmt': '<h1>{{Word}}</h1>',
                 'afmt': '{{FrontSide}}<hr id="answer"><h3>{{Definition}}</h3><br>Synonyms: {{Synonyms}}'
-                        '<br><img src="{{Image}}">',
+                        '<br>{{Image}}',
             },
         ]
     )
@@ -37,7 +37,8 @@ class AnkiDeck:
         image_path = _fetch_image(image_url)
         my_note = genanki.Note(
             model=card_model,
-            fields=[word, dict_info["definition"], ", ".join(dict_info.get("synonyms", "")), image_path]
+            fields=[word, dict_info["definition"], ", ".join(dict_info.get("synonyms", "")),
+                    f"<img src='{image_path}'>"]
         )
         self.deck.add_note(my_note)
         # add an image to deck
@@ -45,7 +46,7 @@ class AnkiDeck:
 
     def output(self):
         self.deck.media_files = self.media_files
-        path = f"_temp/anki_deck_{self.user_id}.apkg"
+        path = f"temp/anki_deck_{self.user_id}.apkg"
         genanki.Package(self.deck).write_to_file(path)
         return path
 
@@ -62,14 +63,14 @@ def _fetch_image(url: str):
     response = requests.get(url, headers=headers)
     file_extension = response.headers["Content-Type"].split("/")[1]
     # write image
-    with open(f"_temp/{url_hash}.{file_extension}", "wb") as f:
+    with open(f"temp/{url_hash}.{file_extension}", "wb") as f:
         f.write(response.content)
-    return f"_temp/{url_hash}.{file_extension}"
+    return f"temp/{url_hash}.{file_extension}"
 
 
 def _check_image_on_local(url_hash: str):
-    if os.path.isfile(f"_temp/{url_hash}.png"):
-        return True, f"_temp/{url_hash}.png"
-    elif os.path.isfile(f"_temp/{url_hash}.jpg"):
-        return True, f"_temp/{url_hash}.jpg"
+    if os.path.isfile(f"temp/{url_hash}.png"):
+        return True, f"temp/{url_hash}.png"
+    elif os.path.isfile(f"temp/{url_hash}.jpg"):
+        return True, f"temp/{url_hash}.jpg"
     return False, None

759ae1408b8071069e81459761946dc4c45011bd
kota113 <iwa124816+keio@gmail.com>
2023-11-06 19:49:16 +0900

    add raise for error

--- dictionary_api.py
+++ dictionary_api.py
@@ -10,5 +10,6 @@ def request(word: str):
         "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
     }
     response = requests.get(url, headers=headers)
+    response.raise_for_status()
     results = response.json()["results"]
     return [i for i in results if "definition" in i]
